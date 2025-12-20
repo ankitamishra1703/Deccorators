c840390c58bff675e886bad86636abba3d8809e1
Ankita Mishra <ankitamishra2617@gmail.com>
2025-12-20 23:39:53 +0530

    Christmass message added

--- deco.py
+++ deco.py
@@ -2,25 +2,28 @@ import random,time
 
 def guess(func):
     def inner():
-        n=6
-        for i in range(1,n):
-            r=random.randint(1,9)
+        for i in range(5,1,-1):
+            r=random.randint(1,5)
             print(r)
             i=int(input("Enter the no:"))
             if i==r: 
+                print("Loading",end='')
                 for i in range(4):
-                    time.sleep(2)              
+                    print("..",end='')
+                    time.sleep(2)
+                print()
+                print("You have gussed the number correctly")              
                 func()
                 break
             else:
-                print(f"You have more {n-2} chances")
-                n-=1                
-                if i==5:
+                print(f"You have more {i-1} chances")                                
+                if i==r:
                     print("You have lost all your chances")
     return inner
 
 
 @guess
 def guss():
-    print("You have gussed the number correctly")
+    
+    print("MERRY CHRISTMASS")
 guss()
